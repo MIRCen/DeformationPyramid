a7361865e988e82a9b16bd26f179ca24568e9efc
Lilian Mehl <lilian.mehll@gmail.com>
2023-06-07 17:39:42 +0200

    Changes for histology data analyse and visualisation

--- shape_transfer.py
+++ shape_transfer.py
@@ -2,170 +2,123 @@ import torch
 import torch.nn as nn
 BCE = nn.BCELoss()
 import open3d as o3d
-
+import logging
+import os
+import time
+from pathlib import Path
 import torch.optim as optim
-
-import  yaml
 from easydict import EasyDict as edict
-
+from model.nets import Deformation_Pyramid
+import torch
 from utils.benchmark_utils import setup_seed
-
 import numpy as np
 
-from model.nets import Deformation_Pyramid
-from model.loss import compute_truncated_chamfer_distance
-import argparse
+from visualisation import print_points_cloud_information, visualize_registration, visualize_label, visualize_deformation
+from data_preparation.data_preparation import read_file_info, inverse_data_normalisation, get_list_ROI
+from data_preparation.dataloaders import  Mouse_pc, get_labels
+from registration_functions import shape_transfer_2D_2D, shape_transfer_multiple_slices_2D_2D, apply_shape_transfer, \
+    save_model, slices_alignment_2D_2D
+import config as cfg
 
 
 setup_seed(0)
 
 
-
+def rotation_features(features, rotation):
+    theta = np.radians(rotation)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array(((c, -s), (s, c)))
+    return np.dot(R, [features[:, 0], features[:, 1]])
 
 if __name__ == "__main__":
-
-    config = {
-        "gpu_mode": True,
-
-        "iters": 500,
-        "lr": 0.01,
-        "max_break_count": 15,
-        "break_threshold_ratio": 0.001,
-
-        "samples": 6000,
-
-        "motion_type": "Sim3",
-        "rotation_format": "euler",
-
-        "m": 9,
-        "k0": -8,
-        "depth": 3,
-        "width": 128,
-        "act_fn": "relu",
-
-        "w_reg": 0,
-        "w_ldmk": 0,
-        "w_cd": 0.1
-    }
-
-    config = edict(config)
-
+    config = edict(cfg.config)
     if config.gpu_mode:
         config.device = torch.cuda.current_device()
     else:
         config.device = torch.device('cpu')
 
+    data_name = "10_TI2202_mouse-NeuN_G-Liot"
+    project_name = "10_brain_region_segmentation"
+    reference_file = "reference"
+    source_file = "source"
+    results_file = "results"
+    data_path = Path(__file__)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', type=str, help= 'Path to the src mesh.')
-    parser.add_argument('-t', type=str, help='Path to the tgt mesh.')
-    args = parser.parse_args()
+    data_path = Path(__file__).parent.joinpath("datasets", data_name, project_name)
+    data_reference_roi_path = data_path.joinpath(reference_file, "57_ROI_img")
+    data_reference_params_path = data_path.joinpath(reference_file, "70_paramap")
+    data_reference_images = data_path.joinpath(reference_file, "20_gis")
+    data_reference_info_slices = data_path.joinpath(reference_file, "info_mouse_slices.ods")
 
+    data_source_roi_path = data_path.joinpath(source_file, "57_ROI_img")
+    data_source_params_path = data_path.joinpath(source_file, "70_paramap")
+    data_source_images = data_path.joinpath(source_file, "20_gis")
+    data_source_info_slices = data_path.joinpath(source_file, "info_mouse_slices.ods")
 
-    S=args.s
-    T=args.t
-
-    """read S, sample pts"""
-    src_mesh = o3d.io.read_triangle_mesh( S )
-    src_mesh.compute_vertex_normals()
-    pcd1 =  src_mesh.sample_points_uniformly(number_of_points=config.samples)
-    pcd1.paint_uniform_color([0, 0.706, 1])
-    src_pcd = np.asarray(pcd1.points, dtype=np.float32)
-
-    o3d.visualization.draw_geometries([src_mesh])
-
-    """read T, sample pts"""
-    tgt_mesh = o3d.io.read_triangle_mesh( T )
-    tgt_mesh.compute_vertex_normals()
-    pcd2 =  tgt_mesh.sample_points_uniformly(number_of_points=config.samples)
-    tgt_pcd = np.asarray(pcd2.points, dtype=np.float32)
-
-    o3d.visualization.draw_geometries([tgt_mesh])
+    data_results_path = data_path.joinpath(results_file)
 
 
-    """load data"""
-    src_pcd, tgt_pcd = map( lambda x: torch.from_numpy(x).to(config.device), [src_pcd, tgt_pcd ] )
+    #load data
+    reference_slices_numeros, reference_rotations, reference_image_dim = read_file_info(data_reference_info_slices)
+    source_slices_numeros, source_rotations, source_image_dim = read_file_info(data_source_info_slices)
 
 
+    #Construct Mouse class
+    Mouse_reference=Mouse_pc(data_reference_params_path,data_ROI_path=data_reference_roi_path,image_sizes=reference_image_dim)
+    Mouse_source = Mouse_pc(data_source_params_path, data_ROI_path=data_source_roi_path,
+                                         image_sizes=source_image_dim, rotation=source_rotations)
 
-    """construct model"""
-    NDP = Deformation_Pyramid(depth=config.depth,
-                              width=config.width,
-                              device=config.device,
-                              k0=config.k0,
-                              m=config.m,
-                              nonrigidity_est=config.w_reg > 0,
-                              rotation_format=config.rotation_format,
-                              motion=config.motion_type)
+    #Choose slices
+    index_start=0
+    index_end=4
+    tgt_slice_interval=Mouse_reference.get_slices_pc_interval(index_start,index_end)
+    src_slice_interval=Mouse_source.get_slices_pc_interval(index_start,index_end)
+    """
+    #Compute shape transfer
+    NDP_list,loss_list=shape_transfer_multiple_slices_2D_2D(tgt_slice_interval, src_slice_interval, m=9,trunc = 0.0002)
 
+    #Save models
+    for i in range(index_start, index_end):
+        save_model(NDP_list[i-index_start],data_results_path,name=i)
+    
+    """
+    #load models
+    NDP_list = []
+    for i in range(index_start, index_end):
 
+        model = torch.load(os.path.join(data_results_path, f'model_{i}.pth'))
+        NDP_list.append(model)
+    #Apply NDP, get labels
+    list_labels=[]
+    list_warped_src=[]
 
-    """cancel global translation"""
-    src_mean = src_pcd.mean(dim=0, keepdims=True)
-    tgt_mean = tgt_pcd.mean(dim=0, keepdims=True)
-    src_pcd = src_pcd - src_mean
-    tgt_pcd = tgt_pcd - tgt_mean
+    for i in range(index_start, index_end):
+        warped_src_b = apply_shape_transfer(NDP_list[i-index_start], Mouse_source.get_slice_pc(i))
+        labeled_nodes=get_labels(Mouse_reference, warped_src_b, i)
+        list_labels.append(labeled_nodes)
+        list_warped_src.append(warped_src_b)
+        #grid = visualize_deformation(NDP_list[i])
 
+    #display 3D segmentation
 
+    aligned_src_pc_list=slices_alignment_2D_2D(src_slice_interval, m=1, motion_type="SE3")
 
+    for index in range(index_start, index_end, 1):
+        slice = aligned_src_pc_list[index]
+        # slice+=index*0.01
+        slice[:, 2] += index * 0.25
 
-    s_sample = src_pcd
-    t_sample = tgt_pcd
+    c_aligned_src_pc_list=np.concatenate(aligned_src_pc_list, axis=0)
+    c_labels= np.concatenate(list_labels, axis = 0)
+    visualize_label(c_aligned_src_pc_list, c_labels, region = 1)
 
-
-    for level in range(NDP.n_hierarchy):
-
-        """freeze non-optimized level"""
-        NDP.gradient_setup(optimized_level=level)
-
-        optimizer = optim.Adam(NDP.pyramid[level].parameters(), lr=config.lr)
-
-        break_counter = 0
-        loss_prev = 1e+6
-
-        """optimize current level"""
-        for iter in range(config.iters):
-
-
-            s_sample_warped, data = NDP.warp(s_sample, max_level=level, min_level=level)
-
-            loss = compute_truncated_chamfer_distance(s_sample_warped[None], t_sample[None], trunc=1e+9)
-
-
-            if level > 0 and config.w_reg > 0:
-                nonrigidity = data[level][1]
-                target = torch.zeros_like(nonrigidity)
-                reg_loss = BCE(nonrigidity, target)
-                loss = loss + config.w_reg * reg_loss
-
-
-            # early stop
-            if loss.item() < 1e-4:
-                break
-            if abs(loss_prev - loss.item()) < loss_prev * config.break_threshold_ratio:
-                break_counter += 1
-            if break_counter >= config.max_break_count:
-                break
-            loss_prev = loss.item()
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-
-        # use warped points for next level
-        s_sample = s_sample_warped.detach()
+    c_slices = Mouse_source.merge_point_cloud(index_start, index_end)
+    c_labels= np.concatenate(list_labels, axis = 0)
+    visualize_label(c_slices, c_labels, region = 1)
 
 
 
-    """warp-original mesh verttices"""
-    NDP.gradient_setup(optimized_level=-1)
-    mesh_vert = torch.from_numpy(np.asarray(src_mesh.vertices, dtype=np.float32)).to(config.device)
-    mesh_vert = mesh_vert - src_mean
-    warped_vert, data = NDP.warp(mesh_vert)
-    warped_vert = warped_vert.detach().cpu().numpy()
-    src_mesh.vertices = o3d.utility.Vector3dVector(warped_vert)
-    o3d.visualization.draw_geometries([src_mesh])
-
-    """dump results"""
-    # o3d.io.write_triangle_mesh("sim3_demo/things4D/" + sname + "-fit.ply", src_mesh)
+    #Display result by slices
+    for i in range(index_end):
+        visualize_registration(Mouse_reference.get_slice_pc(i), Mouse_source.get_slice_pc(i),list_warped_src[i])
+        visualize_label(Mouse_source.get_slice_pc(i), list_labels[i])
